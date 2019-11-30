39efc01f3007284db2dccdcecf0d94cae40350b8
Myles Young <myles.young@mailchimp.com>
2019-11-30 17:53:24 -0500

    Initial Commit

---
+++ MyNoteApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyNoteApp
+//
+//  Created by Myles Young on 11/30/19.
+//  Copyright © 2019 Myles Young. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyNoteApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyNoteApp
+//
+//  Created by Myles Young on 11/30/19.
+//  Copyright © 2019 Myles Young. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
